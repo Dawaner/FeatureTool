1d4bbbaa301236312ea7fca80b559bb518057072
yanghy <yanghy@eternalinfo.cn>
2024-03-20 15:53:14 +0800

    version 1.0.7

--- featureuitls/featureUtils.py
+++ featureuitls/featureUtils.py
@@ -1,58 +1,32 @@
 import numpy as np
 import pandas as pd
 
-from featureuitls import typeUtils
-
-info_flag = "[ALIOTH - INFO - FEATURE_NAME] "  # <feature>为占位符
-error_flag = "[ALIOTH - ERROR] "
-
-
-def print_error_info(info_type):
-    if info_type == "table_name":
-        print(error_flag + "wrong table_name type, please use string")
-    elif info_type == "data":
-        print(error_flag + "wrong data type, please check your data type")
-
 
 def print_info(data, table_name):
-    print(info_flag)
+    print("[ALIOTH - INFO - FEATURE_NAME] ")
     print("[" + ",".join(data) + "]")
     print(table_name)
 
 
 def get_feature_info(data, table_name=""):
     """
-    获取特征信息: 读取第一行/表头并输出
-    :param data: 数组、字典、dataFrame、numpy.array、csv、excel（表格使用路径传入）
-    :param table_name: 表名
-    :return: 输出信息至控制台
+        获取特征信息: 读取第一行/表头并输出
+        :param data: 数组、字典、set、dataFrame、numpy.array、csv、excel（表格使用路径传入）
+        :param table_name: 表名
+        :return: 输出信息至控制台
     """
     # 判断格式
-    table_name_type = typeUtils.get_data_type(table_name)
-    if table_name_type != "string":
-        print_error_info("table_name")
-        return
-    data_type = typeUtils.get_data_type(data)
-    if data_type == "other":
-        print_error_info("data")
-        return
-    else:
-        # 一维数组直接输出
-        try:
-            if data_type == "list" and len(np.array(data).shape) == 1:
-                print_info(list(map(str, data)), table_name)
-                return
-        except Exception as e:
-            print_error_info("data")
-            return
-        # 其他
-        try:
-            # 格式转换
-            df_data = typeUtils.change_type_to_dataframe(data, data_type)
-        except Exception as e:
-            print_error_info("data")
-            return
-        if df_data.empty:
-            print_error_info("data")
-            return
-    print_info(df_data.columns, table_name)
+    data_type = type(data)
+    if data_type == str:
+        if data.endswith(".csv"):
+            print_info(pd.read_csv(data), table_name)
+        elif data.endswith(".xlsx"):
+            print_info(pd.read_excel(data), table_name)
+    elif data_type == list or data_type == set:
+        print_info(list(map(str, data)), table_name)
+    elif data_type == dict:
+        print_info(pd.DataFrame(data).columns, table_name)
+    elif data_type == pd.core.frame.DataFrame:
+        print_info(data.columns, table_name)
+    elif data_type == np.ndarray:
+        print_info(list(map(str, data[0])), table_name)
